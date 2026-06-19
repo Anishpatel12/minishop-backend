6987129b487af76d73afd39fd463bc000c59386c
Anishpatel12 <botkillerkiller994@gmail.com>
2026-06-19 10:33:29 +0530

    Add OTP authentication and email verification

--- services/emailService.js
+++ services/emailService.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter =
   nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user:
         process.env.EMAIL_USER,
@@ -10,7 +12,6 @@ const transporter =
         process.env.EMAIL_PASS,
     },
   });
-
 const sendOTPEmail = async (
   email,
   otp
